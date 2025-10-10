16796820f5ac77f177417523bee7cc42710b91b2
Nacho Martínez <44577104+NachoMC@users.noreply.github.com>
2025-10-10 13:16:15 +0200

    deck thumb imgs

--- script.js
+++ script.js
@@ -288,7 +288,7 @@ function renderPile(pile, title, titleClass) {
         $('#deck-modal-empty').addClass('d-none');
         pile.forEach((c, i) => {
             $container.append(`
-                <img src="images/cards/${c.index}-front.jpg" class="hand-card-thumb" data-idx="${i}" alt="Carta ${c.index}">
+                <img src="images/cards/${c.index}-front-thumb.jpg" class="hand-card-thumb" data-idx="${i}" alt="Carta ${c.index}">
             `);
         });
         $('#deck-modal-cards .hand-card-thumb').off('click').on('click', function () {
