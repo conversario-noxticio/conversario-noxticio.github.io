92b1b93dd9893ccd856b2453621725bcbdf8d704
Nacho Martínez <44577104+NachoMC@users.noreply.github.com>
2026-03-14 16:25:06 +0100

    book

--- book/script.js
+++ book/script.js
@@ -1,32 +1,4 @@
 ﻿
-// Full screen button
-
-const container = document.getElementById('pdfContainer');
-const iframe = document.getElementById('pdfViewer');
-const btn = document.getElementById('fullscreenBtn');
-
-btn.addEventListener('click', async () => {
-    try {
-        if (iframe.requestFullscreen) {
-            await iframe.requestFullscreen();
-        } else if (iframe.webkitRequestFullscreen) { // Safari/iOS
-            await iframe.webkitRequestFullscreen();
-        } else if (iframe.mozRequestFullScreen) { // Firefox
-            await iframe.mozRequestFullScreen();
-        } else if (container.requestFullscreen) {
-            await container.requestFullscreen();
-        }
-        document.body.classList.add('fullscreen-active');
-    } catch (err) {}
-});
-
-document.addEventListener('fullscreenchange', () => {
-    if (!document.fullscreenElement) {
-        document.body.classList.remove('fullscreen-active');
-    }
-});
-
-
 // Loading Spinner
 
 const spinner = document.getElementById('loadingSpinner');
